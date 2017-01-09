021b2025842bc3b8682c68a3d8be47e902fe6ee3
Denis <corvus007@yandex.ru>
2017-01-09 21:29:33 +0300

    Страницы О компании, Услуги, Партнеры

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,9 @@
-console.log('script.js');
+document.addEventListener('DOMContentLoaded', function() {
+  var aboutSlider = document.querySelector('.about-slider');
+  if (aboutSlider) {
+    $(aboutSlider).slick({
+      accessibility: false,
+      infinite: false,
+    });
+  }
+});
