289017609b6e2f163e1f8fcfa428b197978716d7
Denis <corvus007@yandex.ru>
2017-01-23 12:11:37 +0300

    Стилизация карты

--- app/js/script.js
+++ app/js/script.js
@@ -148,7 +148,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Инициализация карты
 function initializeMap() {
-
   var mapLocations = [];
   var locationPlaces = document.querySelectorAll('[data-place-location]');
   var ICONPATH = 'images/svg-icons/pin.svg';
@@ -199,6 +198,178 @@ function initializeMap() {
       scrollwheel: false,
       disableDefaultUI: true,
       zoomControl: true,
+      styles: [
+      {
+        "elementType": "geometry",
+        "stylers": [
+        {
+          "color": "#f5f5f5"
+        }
+        ]
+      },
+      {
+        "elementType": "labels.icon",
+        "stylers": [
+        {
+          "visibility": "off"
+        }
+        ]
+      },
+      {
+        "elementType": "labels.text.fill",
+        "stylers": [
+        {
+          "color": "#616161"
+        }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+        {
+          "color": "#f5f5f5"
+        }
+        ]
+      },
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+        {
+          "color": "#e1e1e1"
+        },
+        {
+          "lightness": 30
+        }
+        ]
+      },
+      {
+        "featureType": "landscape.man_made",
+        "elementType": "labels.text",
+        "stylers": [
+        {
+          "color": "#969696"
+        }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+        {
+          "color": "#eeeeee"
+        }
+        ]
+      },
+      {
+        "featureType": "poi",
+        "elementType": "labels.text.fill",
+        "stylers": [
+        {
+          "color": "#757575"
+        }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+        {
+          "color": "#e5e5e5"
+        }
+        ]
+      },
+      {
+        "featureType": "poi.park",
+        "elementType": "labels.text.fill",
+        "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+        {
+          "color": "#ffffff"
+        }
+        ]
+      },
+      {
+        "featureType": "road.arterial",
+        "elementType": "labels.text.fill",
+        "stylers": [
+        {
+          "color": "#757575"
+        }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+        {
+          "color": "#dadada"
+        }
+        ]
+      },
+      {
+        "featureType": "road.highway",
+        "elementType": "labels.text.fill",
+        "stylers": [
+        {
+          "color": "#616161"
+        }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels.text.fill",
+        "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+        ]
+      },
+      {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [
+        {
+          "color": "#e5e5e5"
+        }
+        ]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "geometry",
+        "stylers": [
+        {
+          "color": "#eeeeee"
+        }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+        {
+          "color": "#c9c9c9"
+        }
+        ]
+      },
+      {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+        ]
+      }
+      ]
     };
   }
 
